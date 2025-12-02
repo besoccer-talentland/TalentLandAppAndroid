58914bc9edb0ddc3394c6d1221e76900c083bdef
Juan Zurita <juanzurita@besoccer.com>
2025-12-02 09:49:27 +0100

    feat: integrate hilt dependency injection

--- app/src/main/java/com/talentland/talentlandappandroid/MainActivity.kt
+++ app/src/main/java/com/talentland/talentlandappandroid/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
+import dagger.hilt.android.AndroidEntryPoint
 import com.talentland.talentlandappandroid.ui.theme.BeSoccerTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/talentland/talentlandappandroid/TalentLandApplication.kt
@@ -0,0 +1,9 @@
+package com.talentland.talentlandappandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TalentLandApplication : Application()
+
+
